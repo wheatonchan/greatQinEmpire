05259941ae1de955ce762b7ff826cd04e74b397c
wheatonchan <909769423@qq.com>
2019-01-16 23:03:49 +0800

    updata

---
+++ js/common.js
@@ -0,0 +1,14 @@
+//var baseUrl = "http://lc.empire/";
+var baseUrl = "http://app.ukacoin.com/";
+var itemUrl ={
+	sign:"6e4e1b9ebbb615869deee9f69bdad912",
+	mfrom:"app",
+	login:"signin",//登录
+	forgot:"forgot",//忘记密码
+	center:"account/profile",//用户中心
+	nikeName:"account/editnickname",
+	reset:"account/reset",
+	eth:"account/eth",
+	inviter:"team/inviter",
+}
+
